ed3b841bb8bf902a7e35eb16f659ac63234ab3d9
alexcanfield13 <111798790+alexcanfield13@users.noreply.github.com>
2024-04-12 15:48:15 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace GithubActionsLab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_ValidCanfield()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_InvalidCanfield()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_NullCanfield()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,5 +31,100 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_ValidCanfield()
+        {
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
+        }
+
+        [Test]
+        public void Subtract_InvalidCanfield()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "b"));
+            Assert.Throws<FormatException>(() => Program.Subtract("b", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("b", "b"));
+        }
+
+        [Test]
+        public void Subtract_NullCanfield()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_ValidCanfield()
+        {
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(7, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
+        }
+
+        [Test]
+        public void Multiply_InvalidCanfield()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "c"));
+            Assert.Throws<FormatException>(() => Program.Multiply("c", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("c", "c"));
+        }
+
+        [Test]
+        public void Multiply_NullCanfield()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_ValidCanfield()
+        {
+            Assert.AreEqual(0.5, Program.Divide("1", "2"));
+            Assert.AreEqual(1.5, Program.Divide("3", "2"));
+            Assert.AreEqual(2, Program.Divide("10", "5"));
+        }
+
+        [Test]
+        public void Divide_InvalidCanfield()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "d"));
+            Assert.Throws<FormatException>(() => Program.Divide("d", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("d", "d"));
+        }
+
+        [Test]
+        public void Divide_NullCanfield()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Power_ValidCanfield()
+        {
+            Assert.AreEqual(1, Program.Power("1", "2"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
+            Assert.AreEqual(78125, Program.Power("5", "7"));
+        }
+
+        [Test]
+        public void Power_InvalidCanfield()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "e"));
+            Assert.Throws<FormatException>(() => Program.Power("e", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("e", "e"));
+        }
+
+        [Test]
+        public void Power_NullCanfield()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
